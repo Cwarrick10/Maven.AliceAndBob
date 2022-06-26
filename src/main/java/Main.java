f076b0ea5e0ebf707fdf6723e7c7ac568a334444
Cwarrick10 <warrickchar@gmail.com>
2022-06-26 13:07:46 -0400

    done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+        String userNameA = "Alice";
+        String  userNameB = "Bob";
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter Your Name.");
+        String name = sc.nextLine();
+        if(name.equals(userNameA)|(name.equals(userNameB))){
+            System.out.print("Hello how are you.");
+
+        }
+        else{
+            System.out.println("GET OUT.");
+        }
+
 
     }
 }
